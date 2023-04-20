3c6648a3355069935a531b5e9efe4b076c57c5e0
Eckey <louisamarie.eckey@empa.ch>
2023-04-20 12:07:47 +0200

    Add personal message and README

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -15,3 +15,5 @@ class Line:
 
 def distance(p1: "Point", p2: "Point") -> float:
     return ((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2) ** 0.5
+def personal_message():
+    print("Hello from <Louisa>!")  
